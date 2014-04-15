1d1d219b635cdd70e0a2708f533e2ae1bc61c8fb
James Breaaley <stitches_22@hotmail.com>
2014-04-15 10:36:08 +0100

    First Working Performance Test - Needs Streamlining - Clearly shows infinispan running 4.5 times faster than Memcached

--- kvstore/src/main/java/org/jboss/narayana/infinispankvstore/PerformnceTestingTest.java
+++ kvstore/src/main/java/org/jboss/narayana/infinispankvstore/PerformnceTestingTest.java
@@ -1,18 +1,35 @@
 package org.jboss.narayana.infinispankvstore;
 
+import java.math.BigInteger;
+
+import io.narayana.perf.PerformanceTester;
+import io.narayana.perf.Result;
+
 import org.junit.Test;
 
-public class PerformnceTestingTest {
+import com.arjuna.ats.arjuna.objectstore.StoreManager;
 
-	PerformanceTester<> tester = new PerformanceTester<>();
+public class PerformnceTestingTest {
 	
 	@Test
 	public void performanceTest() {
+	
+		//Initial Check
+		int threadNum = 20;
+		int jobsCount = 100000;
 		
-		
-		// INFINISPAN implies HotRod Client
+		PerformanceTester<BigInteger> tester = new PerformanceTester<BigInteger>();
 		KVStoreWorker worker = new KVStoreWorker(StoreType.INFINISPAN);
 		
+		
+		Result<BigInteger> opts = new Result<BigInteger>(threadNum, jobsCount);
+		Result<BigInteger> res = tester.measureThroughput(worker, opts);
+		
+		System.out.printf("Test performance for %d!: %d Txs / second (total time: %d ms versus %d ms)%n",
+                opts.getNumberOfCalls(), opts.getThroughput(), opts.getTotalMillis());
+		
+		// Move the store shutdown
+		StoreManager.shutdown();
 		
 	}
 
