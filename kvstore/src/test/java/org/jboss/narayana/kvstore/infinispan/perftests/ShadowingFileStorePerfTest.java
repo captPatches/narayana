a0d1029ba8e915519afd1d2aed786bd51062abf5
James Brealey <stitches_22@hotmail.com>
2014-06-03 11:54:33 +0100

    Corrected DefaultFileSystemStorePerfTest

--- kvstore/src/test/java/org/jboss/narayana/kvstore/infinispan/perftests/ShadowingFileStorePerfTest.java
+++ kvstore/src/test/java/org/jboss/narayana/kvstore/infinispan/perftests/ShadowingFileStorePerfTest.java
@@ -1,9 +1,14 @@
 package org.jboss.narayana.kvstore.infinispan.perftests;
 
-public class ShadowingFileStorePerfTest extends ObjectStorePerfTester {
+public class ShadowingFileStorePerfTest extends MillTester {
 
 	@Override
 	public void setup() {
+		if(runOnMill() ) {
+			System.setProperty("ObjectStoreEnvironmentBean.objectStoreDir",
+					"/work/b3048933/shadowingStore");
+			setMessage("(Default) Shadowing Store (Mill Mode");
+		}
 		setMessage("(Default) Shadowing Store");
 	}
 
