7169bc388917e2dfe0275216e0e9f2009634448d
James Brealey <stitches_22@hotmail.com>
2014-06-16 16:15:31 +0100

    moving some experiments to the Mill

--- kvstore/src/main/java/org/jboss/narayana/kvstore/infinispan/learning/PrintHostname.java
+++ kvstore/src/main/java/org/jboss/narayana/kvstore/infinispan/learning/PrintHostname.java
@@ -1,9 +1,8 @@
 package org.jboss.narayana.kvstore.infinispan.learning;
 
-class PrintHostname
-{
-  public static void main(String args[]) throws java.net.UnknownHostException
-  {
-        System.out.println(java.net.InetAddress.getLocalHost().getHostName());
+class PrintHostname {
+	public static void main(String args[]) throws java.net.UnknownHostException {
+		String name = java.net.InetAddress.getLocalHost().getHostName();
+		System.out.println(name.subSequence(0, 3));
   }
 }
