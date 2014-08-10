f428ff3b36a7103ac795de5e16ae57c3875c12c4
James Brealey <stitches_22@hotmail.com>
2014-08-10 12:45:50 +0100

    Quick Replicated Store Test

---
+++ kvstore/src/main/java/org/jboss/narayana/infinispan/jmhtests/RepQuickJMH.java
@@ -0,0 +1,25 @@
+package org.jboss.narayana.infinispan.jmhtests;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+public class RepQuickJMH {
+	public static void main(String[] args) throws RunnerException {
+		Options opt = new OptionsBuilder()
+				.include(".*Replicated.*")
+				.forks(Opts.f())
+				.measurementIterations(1)
+				.measurementTime(Opts.r())
+				.warmupIterations(Opts.wi())
+				.timeUnit(TimeUnit.SECONDS)
+				.threads(Opts.t())
+				.build();
+
+		new Runner(opt).run();
+	}
+
+}
